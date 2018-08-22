308a94427d0a041aed67deff016b6661256c865e
Ricardo Boff <ricardo.j.boff@hotmail.com>
2018-08-22 20:11:21 -0300

    3º Commit
    3º Commit - Criação da estrutura do WebService

---
+++ WebService/Controllers/UsuarioController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebService.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        [HttpPost]
+        public bool AddUsuario()
+        {
+
+            return true;
+        }
+
+        [HttpGet]
+        public bool ValidarUsuario()
+        {
+            return true;
+        }
+
+
+
+
+    }
+}
