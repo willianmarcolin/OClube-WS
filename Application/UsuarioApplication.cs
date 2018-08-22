daf2a4e33486268e58c1c6bcd76612f4c60c16af
Ricardo Boff <ricardo.j.boff@hotmail.com>
2018-08-22 20:34:47 -0300

    4º Commit
    4º Commit - Adicionado projeto de Application

---
+++ Application/UsuarioApplication.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Repository;
+using Domain;
+using MongoDB.Driver;
+
+namespace Application
+{
+    public class UsuarioApplication
+    {
+    }
+}
